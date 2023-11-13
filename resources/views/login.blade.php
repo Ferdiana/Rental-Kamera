c674746ed49919c6d69be14bb9556b73d69f32ff
Ferdiana <ferdianarasika170@gmail.com>
2023-11-13 15:47:50 +0800

    login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -11,34 +11,34 @@
 </head>
 
 <body>
-    <div class="container-fluid">
-
-        <div class="row justify-content-between mt-5">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">
-                        <h1 class="card-title">Login</h1>
-                    </div>
+    <div class="container-fluid d-flex flex-column justify-content-center" style="height: 100vh;">
+     
+        <div class="row justify-content-around">
+            <div class="col-md-4 align-self-center">
+                <div class="card" style="border: none">
                     <div class="card-body">
+                        <div class="h1 mb-4"> 
+                            Login
+                        </div>
                         @if (Session::has('error'))
                         <div class="alert alert-danger" role="alert">
                             {{Session::get('error')}}
                         </div>   
-                        @endif
-                        <form action="{{ route('login') }}" method="POST">
-                            @csrf
-                            <div class="mb-3">  
-                                <label for="email" class="form-label">Email address</label>
-                                <input type="email" name="email" class="form-control" id="email"
-                                placeholder="name@example.com" required>
+                    @endif
+                    <form action="{{ route('login') }}" method="POST">
+                        @csrf
+                        <div class="mb-3">  
+                            <label for="email" class="form-label">Email address</label>
+                            <input type="email" name="email" class="form-control" id="email"
+                            placeholder="name@example.com" required>
                         </div>
                         <div class="mb-3">
                             <label for="password" class="form-label">Password</label>
                             <input type="password" name="password" class="form-control" id="password"
-                                placeholder="Password" required>
+                            placeholder="Password" required>
                         </div>
                         <div class="mb-3">
-                            <div class="d-grid">
+                            <div class="div">
                                 <button class="btn-primary">Login</button>
                             </div>
                         </div>
@@ -46,10 +46,11 @@
                 </div>
             </div>
         </div>
-        
-        <div class="col-md-4">
+    
+        <div class="col-md-6 border">
             aa
         </div>
+    
         
     </div>
 </body>
